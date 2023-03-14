c4974db86ed5529dbeafb2bb9e6cb5b1454de0a0
ADoyle <adoyle.h@gmail.com>
2023-03-14 17:13:38 +0800

    fix!: update completions/fzf-tab and plugins/ble
    - removed submodule "deps/ble.sh". User will auto download and install ble.sh via `one plugin enable ble`.
    
    Breaking Change:
    
    If using completions/fzf-tab and plugins/ble, you should upgrade one.bash to v0.1.0.
    And re-enable mods via `one <mod> enable` to fix symbol links.
    See [ChangeLog](https://github.com/one-bash/one.bash/blob/main/CHANGELOG.md#v010-2023-03-14-151701-0800).

--- completions/fzf-tab.opt.bash
+++ completions/fzf-tab.opt.bash
@@ -1,11 +1,11 @@
 IFS='' # for multi-line string
 ABOUT='Tab completion using fzf. https://github.com/lincheney/fzf-tab-completion'
 URL=https://github.com/lincheney/fzf-tab-completion.git
-SCRIPT=bash/fzf-bash-completion.sh
+SCRIPT=git/bash/fzf-bash-completion.sh
 ONE_LOAD_PRIORITY=801 # aliases.completion (LOAD_PRIORITY: 800) will reset completion function
 APPEND="cat <<EOF
 bind -x '\"\\t\": fzf_bash_completion'
 _fzf_bash_completion_loading_msg() {
-  printf '%b%s\\n' \"\\\${PS1@P}\" \"\\\${READLINE_LINE}█\"  | tail -n 1
+  printf '%b%s \\n' \"\\\${PS1@P}\" \"\\\${READLINE_LINE}\"  | tail -n 1
 }
 EOF"
---
+++ plugins/ble.opt.bash
@@ -0,0 +1,5 @@
+ABOUT='A full-featured line editor written in pure Bash! Syntax highlighting, auto suggestions'
+URL='https://github.com/akinomyoga/ble.sh.git'
+SCRIPT=dist/share/blesh/ble.sh
+RUN='make -C "$MOD_DATA_DIR/git" install PREFIX="$MOD_DATA_DIR/dist"'
+DEP_CMDS='gawk'
