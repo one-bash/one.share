6397f064b105139efc9b312a5102b0b0e4efeeef
ADoyle <adoyle.h@gmail.com>
2023-03-16 22:08:55 +0800

    fix(plugins/ble): stop prompt "stty: stdout appears redirected, but stdin is the control descriptor"
    See https://github.com/akinomyoga/ble.sh/issues/303

--- plugins/ble.opt.bash
+++ plugins/ble.opt.bash
@@ -1,5 +1,7 @@
 ABOUT='A full-featured line editor written in pure Bash! Syntax highlighting, auto suggestions'
 URL='https://github.com/akinomyoga/ble.sh.git'
-SCRIPT=dist/share/blesh/ble.sh
+APPEND='cat <<EOF
+. $MOD_DATA_DIR/dist/share/blesh/ble.sh &> "\$(tty)"
+EOF'
 RUN='make -C "$MOD_DATA_DIR/git" install PREFIX="$MOD_DATA_DIR/dist"'
 DEP_CMDS='gawk'
