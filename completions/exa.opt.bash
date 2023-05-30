86862adce4e2141661d6b8bfa8efc1ba28995916
ADoyle <adoyle.h@gmail.com>
2023-05-30 19:40:38 +0800

    fix(completion): fix completions for command "l" and "llm"

--- completions/exa.opt.bash
+++ completions/exa.opt.bash
@@ -2,6 +2,5 @@ URL=https://raw.githubusercontent.com/ogham/exa/master/completions/bash/exa
 
 IFS='' # for multi-line string
 APPEND='cat <<EOF
-complete -o filenames -o bashdefault -F _exa ls
-complete -o filenames -o bashdefault -F _exa ll
+complete -o filenames -o bashdefault -F _exa ls ll l llm
 EOF'
