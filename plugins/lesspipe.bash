6fc3c14001a8da19947c12a9d1cc159731667eb2
ADoyle <adoyle.h@gmail.com>
2023-06-10 04:44:58 +0800

    fix(plugin): support lesspipe.sh && use "return 1" to replace "exit 1"

--- plugins/lesspipe.bash
+++ plugins/lesspipe.bash
@@ -1,22 +1,23 @@
 about-plugin '
 Enable highlight for less command.
+Make less more friendly for non-text input files, see lesspipe(1).
+
 Depend on source-highlight.  https://www.gnu.org/software/src-highlite
 To install:
   for MacOS: brew install source-highlight
   for other systems: https://www.gnu.org/software/src-highlite/source-highlight.html#Installation
 '
 
-# make less more friendly for non-text input files, see lesspipe(1)
-if [[ -x /usr/bin/lesspipe ]]; then
-  LESSPIPE_SCRIPT=/usr/bin/lesspipe
-else
-  LESSPIPE_SCRIPT=$(which src-hilite-lesspipe.sh || true)
-fi
+LESSPIPE_SCRIPT=$( \
+  which lesspipe 2>/dev/null || \
+  which lesspipe.sh 2>/dev/null || \
+  which src-hilite-lesspipe.sh 2>/dev/null || \
+  true)
 
-if [[ -n "$LESSPIPE_SCRIPT" ]]; then
+if [[ -x "$LESSPIPE_SCRIPT" ]]; then
   export LESSOPEN="| ${LESSPIPE_SCRIPT} %s"
   export LESS=" -R "
 else
   echo 'Not found command lesspipe.' >&2
-  exit 1
+  return 1
 fi
