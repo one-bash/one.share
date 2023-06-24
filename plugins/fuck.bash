4d6645901d2735540b65ae64f33a34a5ed7a30e2
ADoyle <adoyle.h@gmail.com>
2023-06-24 17:59:03 +0800

    fix: change plugins/fuck.bash to plugins/fuck.opt.bash

--- plugins/fuck.bash
+++
@@ -1,5 +0,0 @@
-about-plugin 'The-fuck, corrects your previous console command. https://github.com/nvbn/thefuck'
-
-one_check_cmd thefuck || return 1
-
-eval "$(thefuck --alias)"
---
+++ plugins/fuck.opt.bash
@@ -0,0 +1,3 @@
+ABOUT='The-fuck, corrects your previous console command. https://github.com/nvbn/thefuck'
+DEP_CMDS='thefuck'
+APPEND="thefuck --alias"
