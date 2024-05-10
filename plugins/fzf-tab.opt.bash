7e1c921277d7d581fab6ba6ad1eb6b538b92b2b7
ADoyle <adoyle.h@gmail.com>
2024-05-11 01:28:15 +0800

    fix(plugins/fzf-tab): use PRIORITY instead of ONE_LOAD_PRIORITY

--- plugins/fzf-tab.opt.bash
+++ plugins/fzf-tab.opt.bash
@@ -2,7 +2,7 @@ IFS='' # for multi-line string
 ABOUT='Tab completion using fzf. https://github.com/lincheney/fzf-tab-completion'
 URL=https://github.com/lincheney/fzf-tab-completion.git
 SCRIPT=git/bash/fzf-bash-completion.sh
-ONE_LOAD_PRIORITY=801 # aliases.completion (LOAD_PRIORITY: 800) will reset completion function
+PRIORITY=801 # aliases.completion (LOAD_PRIORITY: 800) will reset completion function
 APPEND="cat <<EOF
 bind -x '\"\\t\": fzf_bash_completion'
 _fzf_bash_completion_loading_msg() {
