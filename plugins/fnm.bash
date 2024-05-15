ac89db1c93b200741a91cae64d8fbde29fa03939
ADoyle <adoyle.h@gmail.com>
2024-05-16 01:04:41 +0800

    fix(plugins/fnm): add function fnm_upgrade
    that migrate the global npm packages from specific node version to current node

--- plugins/fnm.bash
+++ plugins/fnm.bash
@@ -2,7 +2,15 @@
 about-plugin https://github.com/Schniz/fnm
 
 if one_l.has_not command fnm; then
-  echo 'Command "fnm" is not found. Ensure you have installed it. See https://github.com/Schniz/fnm'
+	echo 'Command "fnm" is not found. Ensure you have installed it. See https://github.com/Schniz/fnm'
 fi
 
 eval "$(fnm env --use-on-cd)"
+
+# Usage: fnm_upgrade 18.4
+# Desc: migrate the global npm packages from specific node version to current node
+fnm_upgrade() {
+	fnm exec --using="$1" npm ls --global --json |
+		jq -r '.dependencies | to_entries[] | .key+"@"+.value.version' |
+		xargs npm i -g
+}
