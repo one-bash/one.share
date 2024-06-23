5b5b9a42b990f4b2c038c16b391809e92b71e3e7
ADoyle <adoyle.h@gmail.com>
2024-06-24 05:46:07 +0800

    fix(bins/diff-so-fancy): use GITHUB_REPO+GITHUB_VERSION+GITHUB_DOWNLOAD instead of install function

--- bins/diff-so-fancy.opt.bash
+++ bins/diff-so-fancy.opt.bash
@@ -1,18 +1,5 @@
 ABOUT='Install diff-so-fancy from https://github.com/so-fancy/diff-so-fancy'
+GITHUB_REPO=so-fancy/diff-so-fancy
+GITHUB_VERSION=latest
+GITHUB_DOWNLOAD=(diff-so-fancy)
 EXPORTS=(diff-so-fancy)
-
-install() {
-  local version=${_VERSION:-latest}
-  local github=${_GITHUB:-so-fancy/diff-so-fancy}
-  local url
-
-  if [[ $version == latest ]]; then
-    url="https://github.com/$github/releases/latest/download/diff-so-fancy"
-  else
-    url="https://github.com/$github/releases/download/$version/diff-so-fancy"
-  fi
-
-  printf 'To download file: %s\n' "$url" >&2
-  curl -Lo "$MOD_DATA_DIR"/diff-so-fancy "$url"
-  chmod +x "$MOD_DATA_DIR"/diff-so-fancy
-}
