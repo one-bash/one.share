614e0a18eae73b8f16712c5b7c758a8b695aee07
ADoyle <adoyle.h@gmail.com>
2024-09-06 16:13:54 +0800

    feat(plugins/python): support rye
    https://github.com/astral-sh/rye

--- plugins/python.bash
+++ plugins/python.bash
@@ -1,33 +1,43 @@
 about-plugin 'python settings.'
 
 _init_python_paths() {
-  local python2_bin python3_bin
+	local python2_bin python3_bin
 
-  if one_l.has command python3; then
-    python3_bin=$(python3 -c 'import site; print(site.USER_BASE)')/bin
-    if [[ -d $python3_bin ]]; then
-      export PATH="$PATH:$python3_bin"
-    fi
-  fi
+	if one_l.has command python3; then
+		python3_bin=$(python3 -c 'import site; print(site.USER_BASE)')/bin
+		if [[ -d $python3_bin ]]; then
+			export PATH="$PATH:$python3_bin"
+		fi
+	fi
 
-  if one_l.has command python2; then
-    python2_bin=$(python2 -c 'import site; print(site.USER_BASE)')/bin
-    if [[ -d $python2_bin ]]; then
-      export PATH="$PATH:$python2_bin"
-    fi
-  fi
+	if one_l.has command python2; then
+		python2_bin=$(python2 -c 'import site; print(site.USER_BASE)')/bin
+		if [[ -d $python2_bin ]]; then
+			export PATH="$PATH:$python2_bin"
+		fi
+	fi
 
-  if one_l.has command pipx; then
-    export PIPX_BIN_DIR="$XDG_CONFIG_HOME/pipx/bin"
-    export PIPX_HOME="$PIPX_BIN_DIR/pipx/pipx"
+	if one_l.has command pipx; then
+		export PIPX_BIN_DIR="$XDG_CONFIG_HOME/pipx/bin"
+		export PIPX_HOME="$PIPX_BIN_DIR/pipx/pipx"
 
-    export PATH="$PATH:$PIPX_BIN_DIR"
-    eval "$(register-python-argcomplete pipx)"
-  fi
+		export PATH="$PATH:$PIPX_BIN_DIR"
+		eval "$(register-python-argcomplete pipx)"
+	fi
 
-  if one_l.has command pyenv; then
-    eval "$(pyenv init -)"
-  fi
+	if [[ -d $HOME/.rye/shims ]]; then
+		# https://github.com/astral-sh/rye
+		case ":${PATH}:" in
+			*:"$HOME/.rye/shims":*) ;;
+			*)
+				export PATH="$HOME/.rye/shims:$PATH"
+				;;
+		esac
+	fi
+
+	if one_l.has command pyenv; then
+		eval "$(pyenv init -)"
+	fi
 }
 
 _init_python_paths
