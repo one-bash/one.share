a7b5fb2e733370288dd546e2c75a7e22fd04c3b1
ADoyle <adoyle.h@gmail.com>
2024-10-03 23:52:08 +0800

    feat: add completion/tmux

---
+++ completion/tmux.opt.bash
@@ -0,0 +1,2 @@
+ABOUT='better tmux completion. https://github.com/imomaliev/tmux-bash-completion'
+SCRIPT=https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/refs/heads/master/completions/tmux
